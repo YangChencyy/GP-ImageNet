591f640e3e9961e72b68d26e62f79342cbd92a96
Yang Chen <rivachen@gl-login2.arc-ts.umich.edu>
2024-02-25 01:14:36 -0500

    update

--- dataset.py
+++ dataset.py
@@ -1,15 +1,187 @@
-from torch.utils.data import Dataset
+import pickle
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+import os
+import random
+from torchvision import transforms
+import torchvision.datasets as datasets
 
-class RemappedDataset(Dataset):
-    def __init__(self, dataset, label_mapping):
-        self.dataset = dataset
-        self.label_mapping = label_mapping
+np.random.seed(42)
 
-    def __getitem__(self, index):
-        img, label = self.dataset[index]
-        # Remap the label
-        remapped_label = self.label_mapping[label]
-        return img, remapped_label
+class ImageNetSubset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        image = self.data[idx]
+        label = self.labels[idx]
+        return image, label
+
+def load_data(data_folder):
+    all_data = []
+    all_labels = []
+    for idx in range(1, 11):  # Assuming there are 10 batches for training
+        batch_file = os.path.join(data_folder, f'train_data_batch_{idx}')
+        with open(batch_file, 'rb') as f:
+            batch = pickle.load(f, encoding='bytes')
+            all_data.append(batch['data'])
+            all_labels.extend(batch['labels'])
+            # print(batch['labels'][0:20])
+    return np.concatenate(all_data), np.array(all_labels)
+
+
+def select_and_remap_classes(data, labels, num_classes=10):
+    unique_classes = np.unique(labels)
+    selected_classes = np.random.choice(unique_classes, num_classes, replace=False)
+    
+    # Create a mapping for the selected classes to [0, num_classes-1]
+    class_mapping = {old_class: new_class for new_class, old_class in enumerate(selected_classes)}
+    
+    # Filter and remap labels
+    selected_indices = [i for i, label in enumerate(labels) if label in selected_classes]
+    remapped_labels = [class_mapping[label] for label in labels[selected_indices]]
+    
+    # Apply selection and remapping
+    selected_data = data[selected_indices]
+    
+    return selected_data, np.array(remapped_labels), class_mapping
+
+
+def load_test_data(data_folder, class_mapping):
+    test_file = os.path.join(data_folder, 'val_data')  # Adjust the file name/path as necessary
+    with open(test_file, 'rb') as f:
+        batch = pickle.load(f, encoding='bytes')
+    x = batch['data']
+    y = batch['labels']
+
+    # Adjust labels to match the training data's remapping and selection
+    y = [class_mapping.get(label, -1) for label in y]  # Subtract 1 since original labels start at 1
+    valid_indices = [i for i, label in enumerate(y) if label >= 0]
+    x = x[valid_indices]
+    y = [y[i] for i in valid_indices]
+
+    return x, y
+
+
+##################################  OOD Datasets   ############################################################
+
+class INaturalistDataLoader:
+    def __init__(self, root_dir, version='2021_train_mini', target_type='full', batch_size=32, download=True):
+        self.root_dir = root_dir
+        self.version = version
+        self.target_type = target_type
+        self.batch_size = batch_size
+        self.download = download
+        self.transform = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def get_data_loader(self, shuffle=True, num_workers=4):
+        # Initialize the INaturalist dataset
+        dataset = datasets.INaturalist(root=self.root_dir, version=self.version, target_type=self.target_type, transform=self.transform, download=self.download)
+
+        # Create and return a DataLoader
+        return DataLoader(dataset, batch_size=self.batch_size, shuffle=shuffle, num_workers=num_workers)
+
+
+
+
+class SUN397DataLoader:
+    def __init__(self, root_dir, batch_size=32, download=True):
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.download = download
+        self.transform = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.target_transform = None  # Define if you need to apply any transformations to the labels
+
+    def get_data_loader(self, shuffle=True, num_workers=4):
+        # Initialize the SUN397 dataset
+        dataset = datasets.SUN397(root=self.root_dir, transform=self.transform, target_transform=self.target_transform, download=self.download)
+
+        # Create and return a DataLoader
+        return DataLoader(dataset, batch_size=self.batch_size, shuffle=shuffle, num_workers=num_workers)
+
+
+
+class Places365DataLoader:
+    def __init__(self, root_dir, split='val', small=True, download=True, batch_size=32):
+        self.root_dir = root_dir
+        self.split = split
+        self.small = small
+        self.download = download
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.target_transform = None  # Optional, define if needed
+
+    def get_data_loader(self, shuffle=True, num_workers=4):
+        # Initialize the Places365 dataset
+        dataset = datasets.Places365(root=self.root_dir, split=self.split, small=self.small,
+                                     download=self.download, transform=self.transform,
+                                     target_transform=self.target_transform)
+
+        # Create and return a DataLoader
+        return DataLoader(dataset, batch_size=self.batch_size, shuffle=shuffle, num_workers=num_workers)
+
+
+class DTDDataLoader:
+    def __init__(self, root_dir, split='train', partition=1, download=False, batch_size=32):
+        self.root_dir = root_dir
+        self.split = split
+        self.partition = partition
+        self.download = download
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+
+    def get_data_loader(self, shuffle=True, num_workers=4):
+        # Initialize the DTD dataset
+        train_dataset = datasets.DTD(root=self.root_dir, split=self.split, partition=self.partition,
+                               transform=self.transform, download=self.download)
+        val_dataset = datasets.DTD(root=self.root_dir, split='val', transform=self.transform, download=True)
+        combined_dataset = ConcatDataset([train_dataset, val_dataset])
+
+
+        # Create and return a DataLoader
+        return DataLoader(combined_dataset, batch_size=self.batch_size, shuffle=shuffle, num_workers=num_workers)
+
+class SVHNDataLoader:
+    def __init__(self, root_dir, split='train', download=True, batch_size=32):
+
+        self.root_dir = root_dir
+        self.split = split
+        self.download = download
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.Resize((32, 32)),  # Resize the image to 32x32 pixels
+            transforms.ToTensor(),  # Convert the image to a PyTorch tensor
+            transforms.Normalize(mean=[0.4377, 0.4438, 0.4728], std=[0.1980, 0.2010, 0.1970])
+            # These normalization values are approximations; adjust as necessary.
+        ])
+        self.target_transform = None  # Optional, define if needed
+
+    def get_data_loader(self, shuffle=True, num_workers=4):
+
+        dataset = datasets.SVHN(root=self.root_dir, split=self.split, download=self.download,
+                                transform=self.transform, target_transform=self.target_transform)
+
+        data_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=shuffle, num_workers=num_workers)
+        return data_loader
--- models.py
+++ models.py
@@ -10,15 +10,16 @@ class CustomResNet(nn.Module):
             *list(pretrained_model.children())[:-1],
             nn.Flatten()
         )
-        self.intermediate = nn.Linear(pretrained_model.fc.in_features, feature_size)
+        # for param in self.features.parameters():
+        #     param.requires_grad = False
+        self.fc1 = nn.Linear(pretrained_model.fc.in_features, 512) # 2048
+        self.fc2 = nn.Linear(512, feature_size)
         self.classifier = nn.Linear(feature_size, num_classes)
         
     def forward(self, x):
         x = self.features(x)
-        x = self.intermediate(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         logits = self.classifier(x)
         return x, logits  # Return both the logits and the intermediate features
 
-# Initialize the custom model
-pretrained_model = models.resnet50(pretrained=True)
-custom_model = CustomResNet(pretrained_model, num_classes=10, feature_size=32)
