4f54db59f5f06bbd2fb4e7a82013d316c5ff6b53
Xiaoyang-Song <xysong@umich.edu>
2024-12-10 09:06:41 -0500

    Update: new cifar100 training

---
+++ jobs/cifar100.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=jhjin1
+#SBATCH --job-name=CF100
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+#SBATCH --output=/scratch/sunwbgt_root/sunwbgt98/xysong/GP-ImageNet/jobs/cifar100.log
+
+python cifar100.py
